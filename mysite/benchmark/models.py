c77d50047cbd0b271f4159e05fac873e32a4e26e
ivystrayed <ivystrayed@gmail.com>
2023-01-31 17:16:08 -0800

    began work on calculated rating system, needs to be completed

--- mysite/benchmark/models.py
+++ mysite/benchmark/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.db.models import Avg
 
 
 class Bench (models.Model):
     img = models.ImageField()
     long = models.DecimalField(max_digits=9, decimal_places=6)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def average_rating(self) -> float:
+        return Rating.objects.filter(bench = self).aggregate(average_rating = Avg('rating'))
+
+
+    def __str__(self):
+        return f"bench # {self.id}: {self.average_rating()}"
 
 class Rating (models.Model):
     bench = models.ForeignKey(Bench, on_delete=models.CASCADE)
@@ -13,6 +21,16 @@ class Rating (models.Model):
     accesibility = models.IntegerField()
     squirrels = models.IntegerField()
     time_spent = models.TimeField()
-    
+
+    def calculateRating(self):
+        rating = 0
+        rating = self.squirrel + self.seclusion + self.view 
+        rating /= 3
+        return rating 
+
+
+    def __str__ (self):
+        return f"{self.bench.id}: {self.rating()}"
+
 
 
